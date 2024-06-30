4f72544b1013ad2249dcd274eb3f00cfe0337e68
Nati Diaz <natudiaz21@gmail.com>
2024-06-30 14:10:33 -0300

    actualizado el back

--- nexus.py
+++ nexus.py
@@ -26,6 +26,18 @@ class Catalogo:
             password=password,
             database=database
         )
+        self.cursor = self.conn.cursor()
+        # Intentamos seleccionar la base de datos
+        try:
+            self.cursor.execute(f"USE {database}")
+        except mysql.connector.Error as err:
+            # Si la base de datos no existe, la creamos
+            if err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+                self.cursor.execute(f"CREATE DATABASE {database}")
+                self.conn.database = database
+            else:
+                raise err
+                
         self.cursor = self.conn.cursor(dictionary=True)
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS personajes (
             ID INT AUTO_INCREMENT PRIMARY KEY,
@@ -33,6 +45,10 @@ class Catalogo:
             descripcion VARCHAR(255) NOT NULL,
             imagen_url VARCHAR(255))''')
         self.conn.commit()
+        
+        # Cerrar el cursor inicial y abrir uno nuevo con el parámetro dictionary=True
+        self.cursor.close()
+        self.cursor = self.conn.cursor(dictionary=True)    
 
     # Agregar personaje
     def agregar_personaje(self, nombre, descripcion, imagen):
@@ -67,7 +83,7 @@ class Catalogo:
             print("Producto no encontrado.")    
 
     def modificar_personaje(self,id, nuevo_nombre, nueva_descripcion, nueva_imagen):
-        sql = "UPDATE personajes SET nombre = %, descripcion = %s, imagen_url = %s WHERE id = %s"
+        sql = "UPDATE personajes SET nombre = %s, descripcion = %s, imagen_url = %s WHERE id = %s"
         valores = (nuevo_nombre, nueva_descripcion, nueva_imagen, id)
         self.cursor.execute(sql, valores)
         self.conn.commit()
@@ -88,35 +104,35 @@ class Catalogo:
 catalogo = Catalogo(host='localhost', user='root', password='', database='nexus')
 
 # Carpeta para guardar las imagenes
-ruta_destino = './imagenes/'
+ruta_destino = './static/imagenes/'
 
-# Llamar al método agregar_personaje correctamente
-#catalogo.agregar_personaje('Eren', 'protagonista de Attack on Titan', 'eren.jpg')
+# # Llamar al método agregar_personaje correctamente
+# catalogo.agregar_personaje('Naruto', 'protagonista de Naruto', 'naruto.jpg')
 
-# # # Consultamos un personaje y lo mostramos
-id_personaje = input("Ingrese el ID del personaje: ")
-try:
-    id_personaje = int(id_personaje)
-    personaje = catalogo.consultar_personaje(id_personaje)
-    if personaje:
-        print(f"personaje encontrado: {personaje['nombre']} - {personaje['descripcion']}")
-    else:
-        print(f'personaje con ID= {id_personaje} no encontrado.')
-except ValueError:
-    print("El ID debe ser un número entero válido.")
+# # # # Consultamos un personaje y lo mostramos
+# id_personaje = input("Ingrese el ID del personaje: ")
+# try:
+#     id_personaje = int(id_personaje)
+#     personaje = catalogo.consultar_personaje(id_personaje)
+#     if personaje:
+#         print(f"personaje encontrado: {personaje['nombre']} - {personaje['descripcion']}")
+#     else:
+#         print(f'personaje con ID= {id_personaje} no encontrado.')
+# except ValueError:
+#     print("El ID debe ser un número entero válido.")
     
-# Listar productos
-personajes = catalogo.listar_personajes()
-for personaje in personajes:
-    print(personaje)
+# # Listar productos
+# personajes = catalogo.listar_personajes()
+# for personaje in personajes:
+#     print(personaje)
  
-print("-" * 120)   
+# print("-" * 120)   
     
 # # Eliminar un personaje
-catalogo.eliminar_personaje(6)
-personajes = catalogo.listar_personajes()
-for personaje in personajes:
-    print(personaje)
+# catalogo.eliminar_personaje(6)
+# personajes = catalogo.listar_personajes()
+# for personaje in personajes:
+#     print(personaje)
     
         
 
@@ -125,33 +141,94 @@ for personaje in personajes:
 
 
 # #probando Flask
-# @app.route("/personajes/<int:codigo>", methods=["GET"])
-# def mostrar_personaje(codigo):
-#     personaje = catalogo.consultar_personaje(codigo)
-#     if personaje:
-#         return jsonify(personaje)
-#     else:
-#         return "Personaje no encontrado", 404
+@app.route("/personajes", methods=["GET"])
+def listar_personajes():
+    personajes = catalogo.listar_personajes()
+    return jsonify(personajes)
 
-# @app.route("/personajes", methods=["POST"])
-# def agregar_personaje():
-#     #Recojo los datos del form
-#     nombre = request.form['nombre']
-#     descripcion = request.form['descripcion']
-#     imagen = request.files['imagen']
-#     nombre_imagen = ""
+@app.route("/personajes/<int:id>", methods=["GET"])
+def mostrar_personajes(id):
+    personaje = catalogo.consultar_personaje(id)
+    if personaje:
+        return jsonify(personaje)
+    else:
+        return "Producto no encontrado", 404
+    
+@app.route("/personajes", methods=["POST"])
+def agregar_personaje():
+    #Recojo los datos del form
+    nombre = request.form['nombre']
+    descripcion = request.form['descripcion']
+    imagen = request.files['imagen_url']
+    nombre_imagen = ""
 
-#     # Genero el nombre de la imagen
-#     nombre_imagen = secure_filename(imagen.filename)
-#     nombre_base, extension = os.path.splitext(nombre_imagen) 
-#     nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}" 
+    # Genero el nombre de la imagen
+    nombre_imagen = secure_filename(imagen.filename)
+    nombre_base, extension = os.path.splitext(nombre_imagen) 
+    nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}"
 
-#     nuevo_codigo = catalogo.agregar_personaje(nombre,descripcion,nombre_imagen)
-#     if nuevo_codigo:    
-#         imagen.save(os.path.join(ruta_destino, nombre_imagen))
-#         return jsonify({"mensaje": "Producto agregado correctamente.", "codigo": nuevo_codigo, "imagen": nombre_imagen}), 201
-#     else:
-#         return jsonify({"mensaje": "Error al agregar el producto."}), 500
+    nuevo_id = catalogo.agregar_personaje(nombre, descripcion, nombre_imagen)
+    if nuevo_id:    
+            imagen.save(os.path.join(ruta_destino, nombre_imagen))
+            return jsonify({"mensaje": "Producto agregado correctamente.", "codigo": nuevo_codigo, "imagen": nombre_imagen}), 201
+    else:
+            return jsonify({"mensaje": "Error al agregar el producto."}), 500    
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+@app.route("/personajes/<int:id>", methods=["PUT"])
+def modificar_producto(id):
+    #Se recuperan los nuevos datos del formulario
+    nuevo_nombre = request.form.get("nombre")
+    nueva_descripcion = request.form.get("descripcion")
+    
+    # Verifica si se proporcionó una nueva imagen
+    if 'imagen' in request.files:
+        imagen = request.files['imagen']
+        # Procesamiento de la imagen
+        nombre_imagen = secure_filename(imagen.filename) 
+        nombre_base, extension = os.path.splitext(nombre_imagen) 
+        nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}" 
+
+        # Guardar la imagen en el servidor
+        imagen.save(os.path.join(ruta_destino, nombre_imagen))
+        
+        # Busco el producto guardado
+        personaje = catalogo.consultar_personaje(id)
+        if personaje: # Si existe el producto...
+            imagen_vieja = personaje["imagen_url"]
+            # Armo la ruta a la imagen
+            ruta_imagen = os.path.join(ruta_destino, imagen_vieja)
+
+            # Y si existe la borro.
+            if os.path.exists(ruta_imagen):
+                os.remove(ruta_imagen)
+    else:     
+        personaje = catalogo.consultar_personaje(id)
+        if personaje:
+            nombre_imagen = personaje["imagen_url"]
+
+   # Se llama al método modificar_producto pasando el codigo del producto y los nuevos datos.
+    if catalogo.modificar_personaje(id, nuevo_nombre, nueva_descripcion, nombre_imagen):
+        return jsonify({"mensaje": "Producto modificado"}), 200
+    else:
+        return jsonify({"mensaje": "Producto no encontrado"}), 403
+
+@app.route("/personajes/<int:id>", methods=["DELETE"])
+def eliminar_personaje(id):
+    # Primero, obtiene la información del producto para encontrar la imagen
+    personaje = catalogo.consultar_personaje(id)
+    if personaje:
+        # Eliminar la imagen asociada si existe
+        ruta_imagen = os.path.join(ruta_destino, personaje['imagen_url'])
+        if os.path.exists(ruta_imagen):
+            os.remove(ruta_imagen)
+
+        # Luego, elimina el producto del catálogo
+        if catalogo.eliminar_personaje(id):
+            return jsonify({"mensaje": "Producto eliminado"}), 200
+        else:
+            return jsonify({"mensaje": "Error al eliminar el producto"}), 500
+    else:
+        return jsonify({"mensaje": "Producto no encontrado"}), 404
+    
+if __name__ == "__main__":
+    app.run(debug=True)
